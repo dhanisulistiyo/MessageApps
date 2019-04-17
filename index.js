119abbb0689d285b56066cae069572383d94e7ab
Dhani Sulistiyo Wibowo <dhani.wibowo@bizzy.co.id>
2019-04-18 02:21:09 +0700

    Create index.js for server

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import http from 'http';
+import { resolvers, typeDefs } from './schema/message'
+
+
+const PORT = process.env.PORT || 3500
+const PORTWS = process.env.PORT || 3501
+const app = express()
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.applyMiddleware({ app })
+
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+app.get('/', (req, res) => {
+  res.send({ hello: 'This Apps' })
+})
+
+app.listen(PORT, () =>{
+  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
+  console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`)
+})
+
